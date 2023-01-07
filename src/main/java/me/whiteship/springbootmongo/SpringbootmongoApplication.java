7c12f42823cb6ab9d705d501dec2e59f1ebff3b4
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-07 22:43:17 +0900

    Initial commit

---
+++ src/main/java/me/whiteship/springbootmongo/SpringbootmongoApplication.java
@@ -0,0 +1,13 @@
+package me.whiteship.springbootmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootmongoApplication.class, args);
+	}
+
+}
