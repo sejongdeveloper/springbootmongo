47f7dfe252de52f3269f32ed02de868e9b2bcc63
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-07 22:56:49 +0900

    스프링 데이터 10부: MongoDB

--- src/main/java/me/whiteship/springbootmongo/SpringbootmongoApplication.java
+++ src/main/java/me/whiteship/springbootmongo/SpringbootmongoApplication.java
@@ -1,13 +1,35 @@
 package me.whiteship.springbootmongo;
 
+import me.whiteship.springbootmongo.account.Account;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootApplication
 public class SpringbootmongoApplication {
 
+	@Autowired
+	MongoTemplate mongoTemplate;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootmongoApplication.class, args);
+	}
+
+	@Bean
+	public ApplicationRunner applicationRunner() {
+		return args -> {
+			Account account = new Account();
+			account.setEmail("aaa@bbb");
+			account.setUsername("aaa");
+			account.setUsername("aaa");
+
+			mongoTemplate.save(account);
+
+			System.out.println("finished");
+		};
 	}
 
 }
